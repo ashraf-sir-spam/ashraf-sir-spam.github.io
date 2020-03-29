743eb3d8cf4568de5d3b36b05a028dc63d9ea65a
Md Ashraful Islam <1605001@ugrad.cse.buet.ac.bd>
2020-03-29 13:18:18 +0600

    Initial setup

---
+++ precache-manifest.88968bf6191eead936d88e3515d3dd39.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5372dd02ed05b1f0c68e",
+    "url": "/css/app.f048c6d0.css"
+  },
+  {
+    "revision": "0dea6066e3ffe4f90cf0",
+    "url": "/css/chunk-4328a93c.5c3e7589.css"
+  },
+  {
+    "revision": "4a24c3d68f519c03b823",
+    "url": "/css/chunk-vendors.73152c73.css"
+  },
+  {
+    "revision": "411b7df13fd85622b13872143bc1ea16",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5372dd02ed05b1f0c68e",
+    "url": "/js/app.68c550b4.js"
+  },
+  {
+    "revision": "28293b1be1303aa7a06d",
+    "url": "/js/chunk-2d0de4ea.1cc9b9d6.js"
+  },
+  {
+    "revision": "0dea6066e3ffe4f90cf0",
+    "url": "/js/chunk-4328a93c.9687c42b.js"
+  },
+  {
+    "revision": "7bc4d27fe877d0faa106",
+    "url": "/js/chunk-4f535df7.78c09180.js"
+  },
+  {
+    "revision": "4a24c3d68f519c03b823",
+    "url": "/js/chunk-vendors.49b16c23.js"
+  },
+  {
+    "revision": "4750933d51425c5cbc9afe4f4809369b",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
